6f8d6b7f26d0198e89e4da69ca858ee5acedcde2
Claus Madsen <claus.nygaard.madsen@gmail.com>
2016-02-09 14:40:49 +0100

    Testing

--- app/src/main/java/com/example/sw804f16/jukebox/MainActivity.java
+++ app/src/main/java/com/example/sw804f16/jukebox/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        int i = 1 + 1; //Junk to test commit.
         return true;
     }
 
