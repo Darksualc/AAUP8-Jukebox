bba3f1e374d12e4d22191ebefeb9a7e064712f63
kharze <mgrege11@student.aau.dk>
2016-03-08 11:31:29 +0100

    Implementet Burger menu with side bar.

--- app/src/main/java/com/example/sw804f16/jukebox/MainActivity.java
+++ app/src/main/java/com/example/sw804f16/jukebox/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.sw804f16.jukebox;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -21,6 +34,10 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 
+import junit.framework.Assert;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements ConnectionStateCallback, PlayerNotificationCallback {
 
     // Replace with your client ID
@@ -31,9 +48,17 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
     private static final int REQUEST_CODE = 1337;
     public boolean connectionFlag = false;
 
-    public boolean isConnectionFlag() {
-        return connectionFlag;
-    }
+    private static String TAG = MainActivity.class.getSimpleName(); //Burger menu item
+    ListView mDrawerList; //Burger menu item
+    RelativeLayout mDrawerPane; //Burger menu item
+    private ActionBarDrawerToggle mDrawerToggle; //Burger menu item
+    private DrawerLayout mDrawerLayout; //Burger menu item
+    ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +88,49 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
         {
             connectionFlag = true;
         }
+
+        mNavItems.add(new NavItem("Home", "Meetup destination", R.drawable.ic_action_home));
+        mNavItems.add(new NavItem("Preferences", "Change your preferences", R.drawable.ic_action_settings));
+        mNavItems.add(new NavItem("About", "Get to know about us", R.drawable.ic_action_about));
+
+        // DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Populate the Navigtion Drawer with options
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        DrawerListAdapter adapter = new DrawerListAdapter(this, mNavItems);
+        mDrawerList.setAdapter(adapter);
+
+        // Drawer Item click listeners
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItemFromDrawer(position);
+            }
+
+            /*
+    * Called when a particular item from the navigation drawer
+    * is selected.
+    * */
+            private void selectItemFromDrawer(int position) {
+                Fragment fragment = new Fragment();
+
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.mainContent, fragment)
+                        .commit();
+
+                mDrawerList.setItemChecked(position, true);
+                setTitle(mNavItems.get(position).mTitle);
+
+                // Close the drawer
+                mDrawerLayout.closeDrawer(mDrawerPane);
+            }
+        });
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true); // Arrow icon
+        getSupportActionBar().setLogo(R.mipmap.burger); // or setLogo()
     }
 
     @Override
@@ -71,6 +139,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -81,6 +150,12 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+        // Pass the event to ActionBarDrawerToggle
+        // If it returns true, then it has handled
+        // the nav drawer indicator touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -101,7 +176,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
                     public void onInitialized(Player player) {
                         player.addConnectionStateCallback(MainActivity.this);
                         player.addPlayerNotificationCallback(MainActivity.this);
-                        player.play("spotify:track:2SUpC3UgKwLVOS2FtZif9N");
+                        //player.play("spotify:track:2SUpC3UgKwLVOS2FtZif9N");
                     }
 
                     @Override
@@ -113,6 +188,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
         }
     }
 
+    //Spotify functions
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
@@ -164,4 +240,67 @@ public class MainActivity extends AppCompatActivity implements ConnectionStateCa
         Spotify.destroyPlayer(this);
         super.onDestroy();
     }
+
+    //-----------Burger Menu---------
+    class NavItem {
+        String mTitle;
+        String mSubtitle;
+        int mIcon;
+
+        public NavItem(String title, String subtitle, int icon) {
+            mTitle = title;
+            mSubtitle = subtitle;
+            mIcon = icon;
+        }
+    }
+
+    class DrawerListAdapter extends BaseAdapter {
+
+        Context mContext;
+        ArrayList<NavItem> mNavItems;
+
+        public DrawerListAdapter(Context context, ArrayList<NavItem> navItems) {
+            mContext = context;
+            mNavItems = navItems;
+        }
+
+        @Override
+        public int getCount() {
+            return mNavItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mNavItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.drawer_item, null);
+            }
+            else {
+                view = convertView;
+            }
+
+            TextView titleView = (TextView) view.findViewById(R.id.title);
+            TextView subtitleView = (TextView) view.findViewById(R.id.subTitle);
+            ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+
+            titleView.setText(mNavItems.get(position).mTitle);
+            subtitleView.setText(mNavItems.get(position).mSubtitle);
+            iconView.setImageResource(mNavItems.get(position).mIcon);
+
+            return view;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/sw804f16/jukebox/PreferencesFragment.java
@@ -0,0 +1,28 @@
+package com.example.sw804f16.jukebox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mike on 08-03-2016.
+ */
+public class PreferencesFragment extends Fragment {
+
+
+    public PreferencesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_preferences, container, false);
+    }
+
+
+}
